65fc01b11e9a3149201d1c915ad1b083a3ebde88
RiggGGSMD <sxjcsybyh@hotmail.com>
2018-05-07 08:26:45 +0800

    add user topic pages

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,7 +4,7 @@
             由 Rigg设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="https://weibo.com/u/6336066897/home?wvr=5&lf=reg"target="_blank"><img src="{{asset('/photos/Weibo_hover.png')}}"/></a></p>
+        <p class="pull-right"><a href="https://weibo.com/u/6336066897?refer_flag=1001030201_"target="_blank"><img src="{{asset('/photos/Weibo_hover.png')}}"/></a></p>
         <p class="pull-right"><a href="mailto:name@email.com"><img src="{{asset('/photos/mail2_hover.png')}}"/></a></p>
         <p class="pull-right"><a href="https://github.com/RiggGGSMD/educationsys"target="_blank"><img src="{{asset('/photos/g-google_hover.png')}}"/></a></p>
     </div>
