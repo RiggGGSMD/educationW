bc4023b7bed2649ad0534813167b2d83ccc4e14c
RiggGGSMD <sxjcsybyh@hotmail.com>
2018-05-09 20:43:17 +0800

    add replies view

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
     public function topics()
     {
         return $this->hasMany(Topic::class);
